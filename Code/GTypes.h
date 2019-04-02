457680ca826451a7f3da0fcbfa6ce93c6196eda1
Jared Plumb <>
2019-04-02 13:38:57 -0600

    Fixed some tabbing

--- Code/GTypes.h
+++ Code/GTypes.h
@@ -236,33 +236,33 @@ enum vkey_t {
 
 class GColor {
 public:
-    /// RGBA format
-    uint32 color;
-    
-    inline GColor ()                                                        : color(0) {}
-    inline GColor (const GColor& c)                                            : color(c.color) {}
-    inline GColor (uint32 hex)                                                : color(hex) {}
-    inline GColor (uint8 r, uint8 g, uint8 b, uint8 a = 0xff)                : color((a) | (b << 8) | (g << 16) | (r << 24)) {}
-    inline GColor (float r, float g, float b, float a = 1.0f)                : color((uint8)(255.0f * a) | ((uint8)(255.0f * b) << 8) | ((uint8)(255.0f * g) << 16) | ((uint8)(255.0f * r) << 24)) {}
-    inline uint8 GetRed () const                                            { return (uint8)((color & 0xff000000) >> 24); }
-    inline uint8 GetGreen () const                                            { return (uint8)((color & 0x00ff0000) >> 16); }
-    inline uint8 GetBlue () const                                            { return (uint8)((color & 0x0000ff00) >> 8); }
-    inline uint8 GetAlpha () const                                            { return (uint8)(color & 0x000000ff); }
-    inline float GetRedF () const                                            { return (float)((uint8)((color & 0xff000000) >> 24) / 255.0f); }
-    inline float GetGreenF () const                                            { return (float)((uint8)((color & 0x00ff0000) >> 16) / 255.0f); }
-    inline float GetBlueF () const                                            { return (float)((uint8)((color & 0x0000ff00) >> 8) / 255.0f); }
-    inline float GetAlphaF () const                                            { return (float)((uint8)(color & 0x000000ff) / 255.0f); }
-    inline void SetRed (uint8 r)                                            { color = (color & 0x00ffffff) | (r << 24); }
-    inline void SetGreen (uint8 g)                                            { color = (color & 0xff00ffff) | (g << 16); }
-    inline void SetBlue (uint8 b)                                            { color = (color & 0xffff00ff) | (b << 8); }
-    inline void SetAlpha (uint8 a)                                            { color = (color & 0xffffff00) | (a); }
-    inline bool operator== (const GColor& c) const                            { return color == c.color; }
-    inline bool operator!= (const GColor& c) const                            { return color != c.color; }
-    inline GColor& operator= (uint32 hex)                                    { color = hex; return *this; }
-    
-    static const uint32 BLACK = 0x000000ff;
-    static const uint32 WHITE = 0xffffffff;
-    static const uint32 CLEAR = 0x00000000;
+	/// RGBA format
+	uint32 color;
+	
+	inline GColor ()                                                        : color(0) {}
+	inline GColor (const GColor& c)                                            : color(c.color) {}
+	inline GColor (uint32 hex)                                                : color(hex) {}
+	inline GColor (uint8 r, uint8 g, uint8 b, uint8 a = 0xff)                : color((a) | (b << 8) | (g << 16) | (r << 24)) {}
+	inline GColor (float r, float g, float b, float a = 1.0f)                : color((uint8)(255.0f * a) | ((uint8)(255.0f * b) << 8) | ((uint8)(255.0f * g) << 16) | ((uint8)(255.0f * r) << 24)) {}
+	inline uint8 GetRed () const                                            { return (uint8)((color & 0xff000000) >> 24); }
+	inline uint8 GetGreen () const                                            { return (uint8)((color & 0x00ff0000) >> 16); }
+	inline uint8 GetBlue () const                                            { return (uint8)((color & 0x0000ff00) >> 8); }
+	inline uint8 GetAlpha () const                                            { return (uint8)(color & 0x000000ff); }
+	inline float GetRedF () const                                            { return (float)((uint8)((color & 0xff000000) >> 24) / 255.0f); }
+	inline float GetGreenF () const                                            { return (float)((uint8)((color & 0x00ff0000) >> 16) / 255.0f); }
+	inline float GetBlueF () const                                            { return (float)((uint8)((color & 0x0000ff00) >> 8) / 255.0f); }
+	inline float GetAlphaF () const                                            { return (float)((uint8)(color & 0x000000ff) / 255.0f); }
+	inline void SetRed (uint8 r)                                            { color = (color & 0x00ffffff) | (r << 24); }
+	inline void SetGreen (uint8 g)                                            { color = (color & 0xff00ffff) | (g << 16); }
+	inline void SetBlue (uint8 b)                                            { color = (color & 0xffff00ff) | (b << 8); }
+	inline void SetAlpha (uint8 a)                                            { color = (color & 0xffffff00) | (a); }
+	inline bool operator== (const GColor& c) const                            { return color == c.color; }
+	inline bool operator!= (const GColor& c) const                            { return color != c.color; }
+	inline GColor& operator= (uint32 hex)                                    { color = hex; return *this; }
+	
+	static const uint32 BLACK = 0x000000ff;
+	static const uint32 WHITE = 0xffffffff;
+	static const uint32 CLEAR = 0x00000000;
 };
 
 
@@ -318,31 +318,31 @@ public:
 
 class GVector {
 public:
-    float_t x, y;
-    
-    inline GVector ()                                                : x((float_t)0), y((float_t)0) {}
-    inline GVector (const GVector& p)                                : x(p.x), y(p.y) {}
-    inline GVector (float_t x_, float_t y_)                            : x(x_), y(y_) {}
-    inline bool operator== (const GVector& p) const                    { return x == p.x && y == p.y; }
-    inline bool operator!= (const GVector& p) const                    { return x != p.x || y != p.y; }
-    inline const GVector operator+ (const GVector& p) const            { return GVector(x + p.x, y + p.y); }
-    inline const GVector operator- () const                            { return GVector(-x, -y); }
-    inline const GVector operator- (const GVector& p) const            { return GVector(x - p.x, y - p.y); }
-    inline const GVector operator* (float_t t) const                    { return GVector(x * t, y * t); }
-    inline const GVector operator/ (float_t t) const                    { return GVector(x / t, y / t); }
-    inline GVector& operator= (const GVector& p)                    { x = p.x; y = p.y; return *this; }
-    inline GVector& operator+= (const GVector& p)                    { x += p.x; y += p.y; return *this; }
-    inline GVector& operator-= (const GVector& p)                    { x -= p.x; y -= p.y; return *this; }
-    inline GVector& operator*= (float_t t)                            { x *= t; y *= t; return *this; }
-    inline GVector& operator/= (float_t t)                            { x /= t; y /= t; return *this; }
-    inline float_t GetDistance (const GVector& v) const                { return sqrt((x - v.x) * (x - v.x) + (y - v.y) * (y - v.y)); }
-    inline float_t GetDot (const GVector& v) const                    { return x * v.x + y * v.y; }
-	inline float_t GetMagnitude () const                                { return sqrt(x * x + y * y); }
-    inline float_t GetLength () const                                { return sqrt(x * x + y * y); } // Yes, Magnitue and Distance are the same
-    inline GVector& Offset (const GVector& p)                        { x += p.x; y += p.y; return *this; }
-    inline GVector& Offset (float_t x_, float_t y_)                    { x += x_; y += y_; return *this; }
-    inline GVector& Normalize ()                                    { float_t t = GetLength(); if(t) { x /= t; y /= t; } return *this; }
-    inline GVector& Reflect (const GVector& n)                        { *this -= (n * ((float_t)2 * GetDot(n))); return *this; }
+	float_t x, y;
+	
+	inline GVector ()												: x((float_t)0), y((float_t)0) {}
+	inline GVector (const GVector& p)								: x(p.x), y(p.y) {}
+	inline GVector (float_t x_, float_t y_)							: x(x_), y(y_) {}
+	inline bool operator== (const GVector& p) const					{ return x == p.x && y == p.y; }
+	inline bool operator!= (const GVector& p) const					{ return x != p.x || y != p.y; }
+	inline const GVector operator+ (const GVector& p) const			{ return GVector(x + p.x, y + p.y); }
+	inline const GVector operator- () const							{ return GVector(-x, -y); }
+	inline const GVector operator- (const GVector& p) const			{ return GVector(x - p.x, y - p.y); }
+	inline const GVector operator* (float_t t) const				{ return GVector(x * t, y * t); }
+	inline const GVector operator/ (float_t t) const				{ return GVector(x / t, y / t); }
+	inline GVector& operator= (const GVector& p)					{ x = p.x; y = p.y; return *this; }
+	inline GVector& operator+= (const GVector& p)					{ x += p.x; y += p.y; return *this; }
+	inline GVector& operator-= (const GVector& p)					{ x -= p.x; y -= p.y; return *this; }
+	inline GVector& operator*= (float_t t)							{ x *= t; y *= t; return *this; }
+	inline GVector& operator/= (float_t t)							{ x /= t; y /= t; return *this; }
+	inline float_t GetDistance (const GVector& v) const				{ return sqrt((x - v.x) * (x - v.x) + (y - v.y) * (y - v.y)); }
+	inline float_t GetDot (const GVector& v) const					{ return x * v.x + y * v.y; }
+	inline float_t GetMagnitude () const							{ return sqrt(x * x + y * y); }
+	inline float_t GetLength () const								{ return sqrt(x * x + y * y); } // Yes, Magnitue and Distance are the same
+	inline GVector& Offset (const GVector& p)						{ x += p.x; y += p.y; return *this; }
+	inline GVector& Offset (float_t x_, float_t y_)					{ x += x_; y += y_; return *this; }
+	inline GVector& Normalize ()									{ float_t t = GetLength(); if(t) { x /= t; y /= t; } return *this; }
+	inline GVector& Reflect (const GVector& n)						{ *this -= (n * ((float_t)2 * GetDot(n))); return *this; }
 };
 
 
