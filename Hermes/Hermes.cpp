4d9d82202a6951dbaf94b4c81dfc91ea9df01c62
Jared Plumb <>
2019-05-08 13:50:33 -0600

    Update to Hermes adding "Data" type

--- Hermes/Hermes.cpp
+++ Hermes/Hermes.cpp
@@ -48,35 +48,48 @@ bool Hermes::Build (const GString& path) {
 		for(uint_t d = 0; d < dir.GetSize(); d++) {
 			GString file = dir.GetFile(d);
 			if((left.IsEmpty() && right.IsEmpty()) || (file.GetLength() >= left.GetLength() + right.GetLength() && GString::strncmp(file, left, left.GetLength()) == 0 && GString::strncmp(file + file.GetLength() - right.GetLength(), right, right.GetLength()) == 0)) {
-				GString name = file;
-				name[name.GetLength() - right.GetLength()] = '\0';
-				GConsole::Print("Building %s %s as ", (const char*)i->second->tag, (const char*)name);
-				name = name + left.GetLength();
-				GConsole::Print("\"%s\"...\n", (const char*)name);
+				GConsole::Print("Building \"%s\" as ", (const char*)file);
+				GString name = file + left.GetLength();				// Removes the path
+				name[name.GetLength() - right.GetLength()] = '\0';	// Removes the extension
 				
 				// All the heavy lifting goes here
 				
 				// Images
 				if(GString::stricmp(i->second->tag, "images") == 0) {
+					GConsole::Print("image \"%s\"...\n", (const char*)name);
 					GImage::Resource image;
 					if(image.NewFromFile(file) == false || image.WriteToPackage(package, name) == false)
 						GConsole::Print("Failed to create %s resource \"%s\"!\n", (const char*)i->second->tag, (const char*)name);
 				
 				// Sounds
 				} else if(GString::stricmp(i->second->tag, "sounds") == 0) {
+					GConsole::Print("sound \"%s\"...\n", (const char*)name);
 					GSound::Resource sound;
 					if(sound.NewFromFile(file) == false || sound.WriteToPackage(package, name) == false)
 						GConsole::Print("Failed to create %s resource \"%s\"!\n", (const char*)i->second->tag, (const char*)name);
 				
 				// Fonts
 				} else if(GString::stricmp(i->second->tag, "fonts") == 0) {
+					GConsole::Print("font \"%s\"...\n", (const char*)name);
 					GFont::Resource font;
 					if(font.NewFromFile(file) == false || font.WriteToPackage(package, name) == false)
 						GConsole::Print("Failed to create %s resource \"%s\"!\n", (const char*)i->second->tag, (const char*)name);
 				
+				// Data
+				} else if(GString::stricmp(i->second->tag, "data") == 0) {
+					GConsole::Print("data \"%s\"...\n", (const char*)(name + right));
+					GFile data;
+					uint8* buffer = NULL;
+					uint_t size = 0;
+					if(data.OpenForRead(file) == false || (size = data.GetSize()) == 0 || (buffer = new uint8[size]) == NULL || 
+					   data.Read(buffer, size) == false || package.Write(name + right, buffer, size) == false)
+						GConsole::Print("Failed to create %s resource \"%s\"!\n", (const char*)i->second->tag, (const char*)name);
+					if(buffer != NULL)
+						delete [] buffer;
+				
 				// Unknown
 				} else {
-					GConsole::Print("Unknown file type \"%s\" for file \"%s\"!\n", (const char*)i->second->tag, (const char*)file);
+					GConsole::Print("unknown file type \"%s\" (file not built)!\n", (const char*)i->second->tag);
 				}
 			}
 		}
