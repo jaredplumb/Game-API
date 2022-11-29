453c765744da4f606b14bd22760e2e3504607562
Jared Plumb <43651036+jaredplumb@users.noreply.github.com>
2022-11-29 12:50:59 -0700

    Moved GFile from GTypes to new file

--- Code/GTypes.h
+++ Code/GTypes.h
@@ -1,8 +1,8 @@
-#ifndef _GTYPES_H_
-#define _GTYPES_H_
+#ifndef GTYPES_H_
+#define GTYPES_H_
 
 #include <cstdint>
-#include <cmath>
+#include <math.h>
 #include <vector>
 
 
@@ -261,55 +261,6 @@ private:
 };
 
 
-/// A GFile is a wrapper class around C-File function calls.  This class uses the
-/// native width versions of the file functions allowing for very large files.
-class GFile {
-public:
-	GFile ();
-	
-	/// This constructor calls OpenForRead
-	GFile (const GString& path);
-	
-	~GFile ();
-	
-	/// Opens a file for reading only
-	bool OpenForRead (const GString& path);
-	
-	/// Opens a file for reading and writing
-	bool OpenForWrite (const GString& path);
-	
-	/// Appends a file for reading and writing
-	bool OpenForAppend (const GString& path);
-	
-	/// Closes this file
-	void Close ();
-	
-	/// Read data from this file
-	bool Read (void* data, int64_t size);
-	
-	/// Write data to this file
-	bool Write (const void* data, int64_t size);
-	
-	/// Is this file currently open
-	bool IsOpen () const;
-	
-	/// Returns the current position for reading and writing in the file
-	int64_t GetPosition () const;
-	
-	/// Returns the size of the file
-	int64_t GetSize () const;
-	
-	/// Sets the reading and writing position in the file
-	bool SetPosition (int64_t position);
-	
-	/// Returns a list of file names in the directory relative to the path sent into the function and all sub directories
-	static std::vector<GString> GetFileNamesInDirectory (const GString& path);
-	
-private:
-	FILE* _file;
-};
-
-
 class GArchive {
 public:
 	static constexpr uint8_t VERSION = 4;
@@ -319,4 +270,4 @@ public:
 };
 
 
-#endif // _GTYPES_H_
+#endif // GTYPES_H_
